76a1b38e6bc133d752266e5498d8891b49a886f0
Lastek <5976416+Lastek@users.noreply.github.com>
2024-09-01 23:27:18 -0600

    Seems to work in vscode well. Not sure about vanilla nvim.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-print("hello treesitter")
+-- print("hello treesitter")
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "java", "javascript", "php", "html", "typescript", "cpp", "python", "c", "lua", "vim", "vimdoc", "query" },
--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Remember to update $XDG_CONFIG_HOME path variable. In this case I set it to %USERPROFILE%!
 -- Do the same for $XDG_DATA_HOME
-print("hello")
-if vim.g.vcode then 	
+-- print("hello")
+if vim.g.vcode then
 	require ("lastek")
 else
 	require("lastek")
